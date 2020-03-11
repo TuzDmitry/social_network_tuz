c6cf142b2e94555102060ecb3d103d12f77269dd
TuzDmitry <dimatuz94@gmail.com>
2020-03-11 09:24:44 +0300

    continue to do structure page....

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import p from './Profile.module.css';
 const Profile=()=> {
     return (
-        <div className={p.content}>Main content
+        <div className={p.content}>
             <div className='photo'>
                 <img src="https://s1.1zoom.ru/b5050/174/USA_Houses_Evening_Pond_Las_Vegas_532588_2880x1800.jpg" alt="your_photo"/>
             </div>
             <div className='ava'>
                 <img src="" alt=""/>
+                <div></div>
             </div>
-            <div className='posts'>fds</div>
-            <div>MY POSTS
+            <div className='posts_block'>MY POSTS
                 <div className='new_post'>New Post</div>
                 <div className='posts'>
                     <div className={p.item}>Post1</div>
---
+++ src/components/Profile_information.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import h from './Header.module.css';
+
+
+const Header=()=> {
+    return (
+        <header className={h.header}>
+            <img src="https://i.pinimg.com/originals/8e/8e/5d/8e8e5d3e0b38ecd0688082625bbf1591.png" alt="fdsfd"/>
+        </header>
+    )
+};
+
+export default Header;
