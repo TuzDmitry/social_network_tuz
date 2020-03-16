d9a48c1713388cb7947af4240c424327e5b028a5
TuzDmitry <dimatuz94@gmail.com>
2020-03-16 15:16:00 +0300

    + Changed <A href='/ths'> =>>>>> component<Navlink to="ths"> , according 20th lesson from it-kamasutra

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -7,7 +7,7 @@ import post from './Post.module.css';
 const Post=(props)=> {
 
     // console.log(props.message);
-debugger;
+// debugger;
     return (
         <div className={post.item}>
             <img src="https://i.pinimg.com/736x/8d/a5/48/8da5485220f54aff3bc0c15dbbd7a739.jpg" alt="fds"/>
