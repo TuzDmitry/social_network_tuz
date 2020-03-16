867244e4c61d28608cb01e4e59f9e97910b0057a
TuzDmitry <dimatuz94@gmail.com>
2020-03-16 15:56:11 +0300

    making tab="Messages" css, according 21th lesson from it-kamasutra

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import s from './Dialogs.module.css';
 const Dialogs=(props)=> {
     return (
-        <div className={s.block}>
-            Dialoooogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog+ ' ' +s.active}>1</div>
+                <div className={s.dialog}>2</div>
+                <div className={s.dialog}>3</div>
+                <div className={s.dialog}>4</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>hi</div>
+                <div className={s.message}>how are u</div>
+                <div className={s.message}>whats up</div>
+            </div>
         </div>
     )
 }
