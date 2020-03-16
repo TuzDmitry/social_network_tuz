83b6c303a5f9625f3198a00231748782e31e9ca5
TuzDmitry <dimatuz94@gmail.com>
2020-03-16 17:01:51 +0300

    Props to the <Dialogs /> accorging to 23 th lesson IT-kamasutra

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,18 +1,42 @@
 import React from "react";
 import s from './Dialogs.module.css';
-const Dialogs=(props)=> {
+import {NavLink} from "react-router-dom";
+
+/////////Вынесли Диалоги с Людьми в отдельную Компоненту////
+const DialogItem = (prors) => {
+    let path = "/dialogs/" + prors.id
+    return (<div className={s.dialog + ' ' + s.active}>
+        <NavLink to={path}>{prors.name}</NavLink>
+    </div>);
+}
+
+/////////Вынесли сообщения в отдельную Компоненту////
+const Message = (props) => {
+    return (
+        <div className={s.message}>{props.message}</div>
+    )
+}
+
+
+const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog+ ' ' +s.active}>1</div>
-                <div className={s.dialog}>2</div>
-                <div className={s.dialog}>3</div>
-                <div className={s.dialog}>4</div>
+                <DialogItem name="Dima" id="1"/>
+                <DialogItem name="Tanya" id="2"/>
+                <DialogItem name="Dasha" id="3"/>
+                <DialogItem name="Kolya" id="4"/>
+                <DialogItem name="Gora" id="5"/>
+                <DialogItem name="Pahsa" id="6"/>
+                {/*<div className={s.dialog}>*/}
+                {/*    <NavLink to="/dialogs/6">Pasha</NavLink>*/}
+                {/*</div>*/}
             </div>
             <div className={s.messages}>
-                <div className={s.message}>hi</div>
-                <div className={s.message}>how are u</div>
-                <div className={s.message}>whats up</div>
+                <Message message="hi"/>
+                <Message message="how are u"/>
+                <Message message="whats up"/>
+                {/*<div className={s.message}>whats up</div>*/}
             </div>
         </div>
     )
